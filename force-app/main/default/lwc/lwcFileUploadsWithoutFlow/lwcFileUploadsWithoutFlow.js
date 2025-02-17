5947a243f18323463367f6801cab46f8becac599
puneetgaur <gaur.puneet@outlook.com>
2025-02-17 15:22:09 -0700

    PGP-0008 - Add Controller classes

--- force-app/main/default/lwc/lwcFileUploadsWithoutFlow/lwcFileUploadsWithoutFlow.js
+++ force-app/main/default/lwc/lwcFileUploadsWithoutFlow/lwcFileUploadsWithoutFlow.js
@@ -5,6 +5,7 @@ import {
 	RefreshEvent,registerRefreshContainer
   } from "lightning/refresh";
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import uploadFiles from "@salesforce/apex/FileUploadController.uploadFiles";
 const columns=[
     { label: 'Path On Client', fieldName: 'PathOnClient', type: 'text' },
     { label: 'File Extension', fieldName: 'FileExtension', type: 'text' },
@@ -31,6 +32,7 @@ export default class LwcFileUploadsWithoutFlow extends LightningElement {
     fileFilterTargetRecords = undefined;
     graphQLUploadedRecords=undefined;
     fileUploadedRecords=undefined;
+    fileFieldValueSeq = undefined
 	closeModal() {
 		this.dispatchEvent(new CloseActionScreenEvent());
 	}
@@ -57,8 +59,20 @@ export default class LwcFileUploadsWithoutFlow extends LightningElement {
             { label: 'One Drive', value: 'One Drive' },
         ];
     }
+    get tabOptions() {
+        return [
+            { label: 'New Document', value: 'New Document' },
+            { label: 'Content Document', value: 'Content Document' },
+            { label: 'FTP Folder', value: 'FTP Folder' },
+            { label: 'S3 Bucket', value: 'S3 Bucket' },
+            { label: 'Google Drive', value: 'Google Drive' },
+            { label: 'Box', value: 'Box' },
+            { label: 'iCloud', value: 'iCloud' },
+            { label: 'One Drive', value: 'One Drive' },
+        ];
+    }
 	get acceptedFormats() {
-        return ['.pdf', '.png', '.csv'];
+        return ['.csv'];
     }
     /*
     @ Getter for contentDocumentLink variable
@@ -130,16 +144,19 @@ export default class LwcFileUploadsWithoutFlow extends LightningElement {
     get contentVersionsVariable(){
         if(!this.contentDocIds) return undefined
         return {
-            contentDocIds: this.contentDocIds
+            contentDocIds: this.contentDocIds,
+            recordUploadId: this.fileFieldValue
         }
     }
     get contentVersionQuery(){
         if(!this.contentDocIds) return undefined
-        return gql `query getContentDocumentLinks($contentDocIds: [ID]) {
+        return gql `query getContentDocumentLinks($contentDocIds: [ID], $recordUploadId: String) {
                 uiapi {
                     query {
                         ContentVersion(
-                            where: { ContentDocumentId: { in: $contentDocIds } },
+                            where: { ContentDocumentId: { in: $contentDocIds }
+                            Record_fileupload__c: { eq: $recordUploadId }
+                            },
                             orderBy: {CreatedDate: {order : DESC}}
                         ) {
                             edges {
@@ -163,6 +180,9 @@ export default class LwcFileUploadsWithoutFlow extends LightningElement {
                                     CreatedDate {
                                     value
                                     }
+                                    Record_fileupload__c{
+                                    value
+                                    }
                                 }
                             }
                         }
@@ -176,6 +196,7 @@ export default class LwcFileUploadsWithoutFlow extends LightningElement {
         this.graphQLContentDocIds = result;
         if(result?.data){
             this.contentDocIds = result.data?.uiapi?.query?.ContentDocumentLink?.edges?.map(edge => edge.node.ContentDocumentId?.value)
+           
         }
         if(result.error){
             this.errors = result.error;
@@ -193,6 +214,7 @@ export default class LwcFileUploadsWithoutFlow extends LightningElement {
                 CreatedDate: edge.node.CreatedDate.value,
             }));
             this.fileFilterTargetRecords = this.contentVersionRecords;
+            this.isFileUploaded = true;
         }
         if(result.error){
             this.errors = result.error;
@@ -214,39 +236,35 @@ export default class LwcFileUploadsWithoutFlow extends LightningElement {
             this.errors = result.error;
         }
     }
-
-    /*get contentDocIds(){
-        if(this.contentDocumentLinks?.data){
-            return this.contentDocumentLinks?.data?.uiapi?.query?.ContentDocumentLink?.edges.map(edge => edge.node?.ContentDocumentId?.value)
-        }
-        return undefined
-    }
-    get contentVersionIds(){
-        if(this.contentVersions?.data){
-            return this.contentVersions?.data?.uiapi?.query?.ContentVersion?.edges.map((edge) => ({
-                Id: edge.node.Id,
-                Title: edge.node.Title.value,
-                FileType: edge.node.FileType.value,
-                PathOnClient: edge.node.PathOnClient.value,
-                FileExtension: edge.node.FileExtension.value,
-                VersionData: edge.node.VersionData.value,
-            }));
-        }
-        return undefined
-    }*/
     uploadFile(event){
         if(!this.isFileUploaded){
-			this.dispatchEvent(
-				new ShowToastEvent({
-					title: 'File Uploaded',
-					message: 'Please upload file before procedding with the next step',	
-					variant: 'success'
-				})
-			);
+            this.invokeToastMessage(null,result["Message"], result["Status"], null, null);
+            return;
 		}
+        var fileContentDocumentWrapperList = [];
+        fileContentDocumentWrapperList = this.fileFilterTargetRecords.map((item) => ({
+            contentVerSionData: item.VersionData,
+            contentFileName: item.PathOnClient
+        }));
+        if(fileContentDocumentWrapperList.length > 0){
+            
+            uploadFiles({targetLocation : "Salesforce", fileContentDocumentWrapperList: fileContentDocumentWrapperList}).then(result => {
+                this.fileUploadedRecords = result;
+                this.isFileUploaded = true;
+                this.invokeToastMessage(null,result["Message"], result["Status"], null, null);
+            }).catch(error => {
+                this.invokeToastMessage(null,JSON.stringify(error), error, null, null);
+            });
+        }
+    }
+    get uploadButtonDisabled(){
+        return !this.isFileUploaded;
+    }
+    handleSelectFile(event){
+        alert(event.target.files);
     }
     handleUploadFinished(event){
-		this.isFileUploaded = true;
+		
         refreshGraphQL(this.graphQLContentDocIds,this.graphQLContentVersionRecords,this.graphQLUploadedRecords);
 		this.dispatchEvent(new RefreshEvent());
 	}
@@ -257,7 +275,6 @@ export default class LwcFileUploadsWithoutFlow extends LightningElement {
 	refreshHandler(){
 		this.isFileUploaded = false;
 		return new Promise(resolve=>{
-            alert('Test');
             refreshGraphQL(this.graphQLContentDocIds,this.graphQLContentVersionRecords,this.graphQLUploadedRecords);
 			resolve(true);
 		})
@@ -277,10 +294,28 @@ export default class LwcFileUploadsWithoutFlow extends LightningElement {
         if(this.tabName != 'Salesforce'){
             this.columns=target_columns;
             this.fileFilterTargetRecords = this.fileUploadedRecords === null || this.fileUploadedRecords === undefined?null : this.fileUploadedRecords.filter((file) => file.Target === this.tabName);
-            console.log('this.fileFilterTargetRecords=', JSON.stringify(this.fileFilterTargetRecords));
         }else{
             this.columns=columns;
             this.fileFilterTargetRecords = this.contentVersionRecords;
         }
+    }
+    handleChange(event){
+        this.fileFieldValue = event.target.value;
+    }
+    get fileFieldValue(){        
+        if(this.fileFieldValueSeq)
+            return this.fileFieldValueSeq;
+        this.fileFieldValueSeq = (parseInt(Math.random() * 100000000)).toString();
+        return this.fileFieldValueSeq;
+    }
+    invokeToastMessage(title, message , variant , messageData , mode){
+        const toastEvent = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant,
+            messageData: messageData??null,
+            mode: mode
+        });
+        this.dispatchEvent(toastEvent);
     }
 }
