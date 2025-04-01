b3cf2a2356173f86739e30443174afbcede8e2a6
puneetgaur <gaur.puneet@outlook.com>
2025-03-31 17:33:55 -0700

    PGP-0016 - Add Component Refresh Platform Event

--- force-app/main/default/lwc/patientEvaluationDetails/patientEvaluationDetails.js
+++ force-app/main/default/lwc/patientEvaluationDetails/patientEvaluationDetails.js
@@ -1,13 +1,32 @@
 import { LightningElement, api, wire , track } from "lwc";
 import EvaluationImage from '@salesforce/resourceUrl/EvaluationImage'
 import getEvaluationComponents from "@salesforce/apex/PatientEvaluationComponents.getEvaluationComponents";
+import {refreshApex} from '@salesforce/apex';
+import {RefreshEvent, registerRefreshHandler, unregisterRefreshHandler} from 'lightning/refresh';
+import {
+	subscribe,
+	unsubscribe,
+	onError,
+	setDebugFlag,
+	isEmpEnabled
+} from 'lightning/empApi';
 export default class PatientEvaluationDetails extends LightningElement {
 	@api recordId;
 	@api objectApiName;
 	@track storeEvaluationComponents;
+	@track storeEvaluationComponentResults;
 	evaluationsImage = EvaluationImage;	
+	subscription={}
+	channelName = '/event/Lightning_Component_Refresh__e';
+	isSubscribeDisabled = false;
+	isUnsubscribeDisabled = !this.isSubscribeDisabled;
+	
 	@wire(getEvaluationComponents, { recordId: "$recordId" })
-	evaluationComponents({ error, data }) {
+	evaluationComponents(result) {
+		this.storeEvaluationComponentResults = result;
+		console.log('result=',result);
+		let data = result.data;
+		let error = result.error;
 		if (data) {
 			let readEvaluationComponents = [];
 			let imageStaticPath = this.evaluationsImage;
@@ -28,4 +47,40 @@ export default class PatientEvaluationDetails extends LightningElement {
 			this.error = error;
 			this.evaluationComponents = undefined;
 		}}
+		connectedCallback(){
+			this.handleSubscribe();
+		}
+		disconnectedCallback(){
+			this.handleUnsubscribe();
+		}
+		registerErrorListener(){
+			onError((error)=>{
+				console.log('Received error from server', JSON.stringify(error))
+			});
+		}
+		handleSubscribe(){
+			const self = this;
+			const messageCallback = (response) => {
+				console.log('Subscription sent to =>',JSON.stringify(response));
+				self.refreshData();
+			};
+			subscribe(this.channelName, -1 ,messageCallback).then((response)=>{
+				console.log('Subscription sent to2 =>',JSON.stringify(response));
+				this.subscription = response;
+			}).catch((error)=>{
+				console.log('Subscription failed =>',JSON.stringify(error));
+			});
+		}
+		handleUnsubscribe(){
+			unsubscribe(this.subscription,(response)=>{
+				console.log('Unsubscribed Successfully');
+				
+			});
+		}
+		refreshPage(){
+			this.dispatchEvent(new RefreshEvent());
+		}
+		refreshData(){
+			refreshApex(this.storeEvaluationComponentResults);
+		}
 }
